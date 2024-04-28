35a71739aa006a38c86358913ea642e83fa5ee28
QuanDN22 <dinhngocquan112378@gmail.com>
2024-04-28 07:21:04 +0700

    update add_data.go and connectiion_DB.go

--- models/connection_to_DB.go
+++ models/connection_to_DB.go
@@ -8,10 +8,10 @@ import (
 )
 
 // connect to database
-func Connection_DB() (*gorm.DB) {
+func Connection_DB() *gorm.DB {
 	dsn := "host=localhost user=postgres password=quan1234 dbname=Server-Management-System port=5432 sslmode=disable TimeZone=Asia/Shanghai"
-	// db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{Logger: logger.Default.LogMode(logger.Info)})
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{Logger: logger.Default.LogMode(logger.Info)})
 
 	if err != nil {
 		log.Fatalf("failed to connect to database: %v", err)
