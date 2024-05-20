cc1cc4c04410b6ad48467a87dbff8f7141baa0ac
QuanDN22 <dinhngocquan112378@gmail.com>
2024-05-20 20:48:12 +0700

    update model server

--- internal/managemen-server/domain/models.go
+++ internal/managemen-server/domain/models.go
@@ -1,10 +1,15 @@
 package domain
 
-import "gorm.io/gorm"
+import "time"
 
 type Server struct {
-	gorm.Model
+	// gorm.Model
+	ID uint `json:"id" gorm:"primarykey"`
+
 	Server_Name   string `json:"server_name" gorm:"column:server_name; not null; unique"`
 	Server_IPv4   string `json:"server_ipv4" gorm:"column:server_ipv4; not null; unique"`
 	Server_Status bool   `json:"server_status" gorm:"column:server_status; not null"`
+
+	CreatedAt time.Time `json:"created_at" gorm:"column:created_at; not null;"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"column:updated_at; not null;"`
 }
